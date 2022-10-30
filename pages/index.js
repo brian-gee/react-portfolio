b4b32d204abacd77e63a732945f58db3d083a575
brian-gee <brian86753099@gmail.com>
2022-10-30 01:35:10 -0400

    tailwind added and created basic nav

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import {BsFillMoonStarsFill} from 'react-icons/bs';
 
 export default function Home() {
   return (
@@ -9,7 +10,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className='bg-white px-10'>
+        <section className=' min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl font-Iosevka'>Brian Gee</h1>
+            <ul className='flex items-center'>
+              <li>
+                <BsFillMoonStarsFill className='cursor-pointer text-2xl' />
+              </li>
+              <li>
+                <a className=' bg-gradient-to-r bg-cyan-400 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a>
+              </li>
+            </ul>
+          </nav>
+
+        </section>
       </main>
     </div>
   )

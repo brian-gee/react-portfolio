dfa9f4da072fd344f9a5fd04d4e258152ac6e13d
brian-gee <brian86753099@gmail.com>
2022-10-30 18:25:47 -0400

    adding more pages

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import {BsLinkedin, BsGithub} from 'react-icons/bs';
 export default function Footer(){
   return (
     <div className='bg-white p-10 dark:bg-black'>
-      <footer className="pt-20">
+      <footer className="pt-20 mt-auto">
           <div className="text-5xl flex justify-center py-3 text-gray-600 space-x-10">
             <a href="https://www.linkedin.com/in/brian-geertsma-983052240/"><BsLinkedin /></a>
             <a href="https://github.com/brian-gee"><BsGithub  /></a>
--- components/nav.js
+++ components/nav.js
@@ -1,21 +1,28 @@
 import {BsFillMoonStarsFill} from 'react-icons/bs';
+import Link from 'next/link';
 
 export default function Nav(){
   return (
     <div className='bg-white px-10 dark:bg-black'>
       <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl font-Iosevka dark:text-white">Brian Gee</h1>
-            <ul className="flex items-center">
-              <li>
-                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl dark:text-white" />
-              </li>
-              <li>
-                <a className=" bg-gradient-to-r bg-cyan-400 text-white px-4 py-2 rounded-md ml-8" href="#">
-                  Resume
-                </a>
-              </li>
-            </ul>
-          </nav>
+        <Link href='/'>
+          <h1  className="text-xl font-Iosevka dark:text-white">Brian Gee</h1>
+        </Link>
+          <ul className="flex items-center">
+            <li>
+              <a className=" bg-gradient-to-r bg-cyan-400 text-white px-4 py-2 rounded-md ml-5" href="/portfolio">
+               Portfolio
+              </a>
+              <a className=" bg-gradient-to-r bg-cyan-400 text-white px-4 py-2 rounded-md ml-5" href="/contact">
+               Contact
+              </a>
+              <a className=" bg-gradient-to-r bg-cyan-400 text-white px-4 py-2 rounded-md ml-5" href="/resume">
+                Resume
+              </a>
+
+            </li>
+          </ul>
+        </nav>
     </div>
   )
 }

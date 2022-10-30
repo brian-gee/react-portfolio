70dd146cba548f8408fbd5e5d96bd508ede165a9
brian-gee <brian86753099@gmail.com>
2022-10-30 14:53:28 -0400

    working with dark mode changes

--- pages/index.js
+++ pages/index.js
@@ -19,16 +19,13 @@ export default function Home() {
       <main className="bg-white px-10 dark:bg-black">
         <section className=" min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl font-Iosevka">Brian Gee</h1>
+            <h1 className="text-xl font-Iosevka dark:text-white">Brian Gee</h1>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl" />
+                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl dark:text-white" />
               </li>
               <li>
-                <a
-                  className=" bg-gradient-to-r bg-cyan-400 text-white px-4 py-2 rounded-md ml-8"
-                  href="#"
-                >
+                <a className=" bg-gradient-to-r bg-cyan-400 text-white px-4 py-2 rounded-md ml-8" href="#">
                   Resume
                 </a>
               </li>

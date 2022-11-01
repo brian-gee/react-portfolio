8de750cfe9c43abe520f7a57d7a4fd2036da13e5
Brian <brian86753099@gmail.com>
2022-10-31 21:49:31 -0400

    added note-taker to portfolio and formatting

--- components/footer.js
+++ components/footer.js
@@ -2,12 +2,14 @@ import {BsLinkedin, BsGithub} from 'react-icons/bs';
 
 export default function Footer(){
   return (
-    <div className='bg-white p-10 dark:bg-black'>
+    <div className='bg-white p-10'>
       <footer className="pt-20 mt-auto">
           <div className="text-5xl flex justify-center py-3 text-gray-600 space-x-10">
             <a href="https://www.linkedin.com/in/brian-geertsma-983052240/"><BsLinkedin /></a>
             <a href="https://github.com/brian-gee"><BsGithub  /></a>
           </div>
+          <span class="flex justify-center pt-5 text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022&nbsp;<a href="https://briang.xyz/" class="hover:underline"> Brian Geertsma</a>. All Rights Reserved.
+          </span>
         </footer>
     </div>
   )
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import workday from '../public/workday.png'
 import jate from '../public/jate.png'
+import noteTaker from '../public/note-taker.png'
 import { useState } from 'react';
 
 export default function Portfolio() {
@@ -22,19 +23,27 @@ export default function Portfolio() {
             <h3 className="text-3xl py-1">Portfolio</h3>
           </div>
           <div className="flex flex-col justify-center lg:flex-row lg:flex-wrap">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+            <div className="text-center shadow-lg p-10 rounded-xl m-5">
               <div className='basis-1/3 flex-1'>
-                <h1 className='text-xl'>Work Day Scheduler</h1>
+                <h1 className='text-xl mb-5'>Work Day Scheduler</h1>
                 <Link href='https://github.com/brian-gee/work-day-scheduler'>
                   <Image src={workday} className='rounded-lg object- cover' width={200} height={2} />
                 </Link>
               </div>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+            <div className="text-center shadow-lg p-10 rounded-xl m-5">
               <div className='basis-1/3 flex-1'>
-                <h1 className='text-xl'>JATE</h1>
+                <h1 className='text-xl mb-5'>JATE</h1>
                 <Link href='https://github.com/brian-gee/pwa-text-editor'>
                   <Image src={jate} className='rounded-lg object- cover' width={200} height={200} />
+                </Link>
+              </div>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl m-5">
+              <div className='basis-1/3 flex-1'>
+                <h1 className='text-xl mb-5'>Note Taker</h1>
+                <Link href='https://github.com/brian-gee/'>
+                  <Image src={noteTaker} className='rounded-lg object- cover' width={200} height={200} />
                 </Link>
               </div>
             </div>

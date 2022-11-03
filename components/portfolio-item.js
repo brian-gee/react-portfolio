5f15ab3273ac7842ed4b040ebcb431b970e03178
brian-gee <brian86753099@gmail.com>
2022-11-03 19:14:24 -0400

    export function within declaration

--- components/portfolio-item.js
+++ components/portfolio-item.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-function portfolioItem(params) {
+export default function portfolioItem(params) {
   const { portfolioItems } = params;
 
   return (
@@ -21,5 +21,3 @@ function portfolioItem(params) {
     </div>
   )
 }
-
-export default portfolioItem;

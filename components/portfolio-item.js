a6ba2d6d7702b50087424eff29e2c3737f1192bc
brian-gee <brian86753099@gmail.com>
2022-11-05 00:48:28 -0400

    portfolio-item component working

--- components/portfolio-item.js
+++ components/portfolio-item.js
@@ -1,15 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function portfolioItem(params) {
+export default function PortfolioItem(params) {
   const { portfolioItems } = params;
 
   return (
-    <div>
+    <div className="flex flex-col justify-center lg:flex-row lg:flex-wrap">
       {portfolioItems.map((portfolioItem, i) => (
-        <div key={i} className="">
+        <div key={i} className="columns">
           <Link href={portfolioItem.github}>
-            <h1>Hello</h1>
               <div class="p-5 max-w-sm bg-white rounded-lg shadow-lg m-2">
                   <Image src={portfolioItem.image} alt={portfolioItem.name} className='object-cover'/>
                 <div class="pt-5">
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,8 +1,6 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
 import { useState } from 'react';
-import portfolioItem from '../components/portfolio-item';
+import PortfolioItem from '../components/portfolio-item';
 import workday from '../public/workday.png'
 import jate from '../public/jate.png'
 import noteTaker from '../public/note-taker.png'
@@ -11,9 +9,24 @@ import employeeManager from '../public/employee-manager.png'
 export default function Portfolio() {
   const [portfolioItems] = useState([
     {
-      'name': 'Workday',
-      'image': 'workday',
-      'github': 'github.com'
+      'name': 'Work Day Scheduler',
+      'image': workday,
+      'github': 'https://github.com/brian-gee/work-day-scheduler'
+    },
+    {
+      'name': 'JATE',
+      'image': jate,
+      'github': ''
+    },
+    {
+      'name': 'Note Taker',
+      'image': noteTaker,
+      'github': ''
+    },
+    {
+      'name': 'Employee Manager',
+      'image': employeeManager,
+      'github': ''
     },
   ])
 
@@ -29,44 +42,8 @@ export default function Portfolio() {
           <div className="text-center">
             <h3 className="text-3xl py-1">Portfolio</h3>
           </div>
-          <div className="flex flex-col justify-center lg:flex-row lg:flex-wrap">
-            <portfolioItem portfolioItems = {portfolioItems} />
-          {/* Work Day Scheduler Card */}
-            <Link href="#">
-              <div class="p-5 max-w-sm bg-white rounded-lg shadow-lg m-2">
-                  <Image src={workday} alt='work day scheduler' className='object-cover'/>
-                <div class="pt-5">
-                  <h5 class="mb-2 text-xl text-center tracking-tight">Work Day Scheduler</h5>
-                </div>
-             </div>
-            </Link>
-          {/* JATE Card*/}
-            <Link href="#">
-              <div class="p-5 max-w-sm bg-white rounded-lg shadow-lg m-2">
-                  <Image src={jate} alt='jate' className='object-cover'/>
-                <div class="pt-5">
-                  <h5 class="text-xl text-center tracking-tight">JATE</h5>
-                </div>
-             </div>
-            </Link>
-          {/* Note Taker Card*/}
-            <Link href="#">
-              <div class="p-5 max-w-sm bg-white rounded-lg shadow-lg m-2">
-                  <Image src={noteTaker} alt='note taker' className='object-cover'/>
-                <div class="pt-5">
-                  <h5 class="text-xl text-center tracking-tight">Note Taker</h5>
-                </div>
-             </div>
-            </Link>
-            {/* Employee Manager */}
-            <Link href="#">
-              <div class="p-5 max-w-sm bg-white rounded-lg shadow-lg m-2">
-                  <Image src={employeeManager} alt='employee manager' className='object-cover'/>
-                <div class="pt-5">
-                  <h5 class="text-xl text-center tracking-tight">Note Taker</h5>
-                </div>
-             </div>
-            </Link>
+          <div className="flex justify-center">
+            <PortfolioItem portfolioItems = {portfolioItems} />
           </div>
         </section>
       </main>

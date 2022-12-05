d5fc3bc368f95eceb4010bd858ddd355c3fb3349
brian-gee <brian86753099@gmail.com>
2022-12-05 17:59:08 -0500

    larger text size when hovering footer icons

--- components/footer.js
+++ components/footer.js
@@ -4,16 +4,19 @@ export default function Footer() {
 	return (
 		<footer className="pt-20 mt-auto">
 			<div className="text-5xl flex justify-center py-3 space-x-10">
-				<a href="https://github.com/brian-gee">
+				<a href="https://github.com/brian-gee" className="hover:text-6xl">
 					<BsGithub />
 				</a>
-				<a href="https://www.linkedin.com/in/brian-geertsma/">
+				<a
+					href="https://www.linkedin.com/in/brian-geertsma/"
+					className="hover:text-6xl"
+				>
 					<BsLinkedin />
 				</a>
 			</div>
 			<span className="flex justify-center py-5 text-sm sm:text-center">
 				© 2022&nbsp;
-				<a href="https://briang.xyz/" classname="hover:underline">
+				<a href="https://briang.xyz/" className="hover:underline">
 					Brian Geertsma
 				</a>
 				. All Rights Reserved.

5b0d34e0fff1fd71667e05adcee2ed6a0df31836
brian-gee <brian86753099@gmail.com>
2022-12-15 23:05:41 -0500

    updating call to class not className

--- components/portfolio-item.js
+++ components/portfolio-item.js
@@ -12,9 +12,9 @@ export default function PortfolioItem(params) {
 					<div class="px-6 py-4 flex justify-center">
 						<div class="font-bold text-xl">{portfolioItem.name}</div>
 					</div>
-					<div class="max-w-sm h-72 rounded overflow-hidden shadow-lg p-5">
+					<div className="max-w-sm h-72 rounded overflow-hidden shadow-lg p-5">
 						<Image
-							class="object-cover h-96 rounded"
+							className="object-cover h-96 rounded"
 							src={portfolioItem.image}
 							alt={portfolioItem.name}
 						/>

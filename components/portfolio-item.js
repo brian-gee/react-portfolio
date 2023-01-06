b6986d6c808f8714a7d156e750b7e93c49c95d67
brian-gee <brian86753099@gmail.com>
2023-01-06 00:14:41 -0500

    removing all borders

--- components/portfolio-item.js
+++ components/portfolio-item.js
@@ -8,14 +8,14 @@ export default function PortfolioItem(params) {
 		<div className="max-w-4xl">
 			{portfolioItems.map((portfolioItem, i) => (
 				<Link key={i} href={portfolioItem.link}>
-				<div className="grid grid-cols-2 shadow-lg m-5 border-2 border-light-blue">
+				<div className="grid grid-cols-2 shadow-lg m-5">
 					<div className="px-6 py-4">
 						<div className="font-bold text-sm md:text-lg">{portfolioItem.name}</div>
 						<div className="text-xs md:text-sm">{portfolioItem.desc}</div>
 					</div>
-					<div className="px-4 border-2 border-light-blue">
+					<div className="px-4">
 						<Image
-							className="object-contain h-96 border-2 border-light-blue max-h-72"
+							className="object-contain h-60 max-h-60 my-2"
 							src={portfolioItem.image}
 							alt={portfolioItem.name}
 						/>

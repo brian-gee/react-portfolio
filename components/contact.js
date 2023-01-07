54bcfa27393a7cc7f9911ee4c4a260134939809d
brian-gee <brian86753099@gmail.com>
2023-01-06 21:47:05 -0500

    updating className

--- components/contact.js
+++ components/contact.js
@@ -3,22 +3,22 @@ export default function Portfolio() {
 		<div>
 			<main className="px-10">
 				<section>
-					<div class="relative flex items-top justify-center min-h-screen bg-white sm:items-center sm:pt-0">
-						<div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-							<div class="overflow-hidden">
-								<div class="grid grid-cols-1 md:grid-cols-2">
-									<div class="p-6 mr-2  sm:rounded-lg">
-										<h1 class="text-4xl sm:text-5xl text-gray-800 font-extrabold tracking-tight">
+					<div className="relative flex items-top justify-center min-h-screen bg-white sm:items-center sm:pt-0">
+						<div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+							<div className="overflow-hidden">
+								<div className="grid grid-cols-1 md:grid-cols-2">
+									<div className="p-6 mr-2  sm:rounded-lg">
+										<h1 className="text-4xl sm:text-5xl text-gray-800 font-extrabold tracking-tight">
 											Get in touch
 										</h1>
-										<p class="text-normal text-lg sm:text-2xl font-medium text-gray-600 mt-2">
+										<p className="text-normal text-lg sm:text-2xl font-medium text-gray-600 mt-2">
 											Fill in the form to start a conversation
 										</p>
 									</div>
 
-									<form class="p-6 flex flex-col justify-center">
-										<div class="flex flex-col">
-											<label for="name" class="hidden">
+									<form className="p-6 flex flex-col justify-center">
+										<div className="flex flex-col">
+											<label for="name" className="hidden">
 												Full Name
 											</label>
 											<input
@@ -26,12 +26,12 @@ export default function Portfolio() {
 												name="name"
 												id="name"
 												placeholder="Full Name"
-												class="w-100 mt-2 py-3 px-3 rounded-lg bg-white border-2 border-gray-400 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
+												className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border-2 border-gray-400 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
 											/>
 										</div>
 
-										<div class="flex flex-col mt-2">
-											<label for="email" class="hidden">
+										<div className="flex flex-col mt-2">
+											<label for="email" className="hidden">
 												Email
 											</label>
 											<input
@@ -39,12 +39,12 @@ export default function Portfolio() {
 												name="email"
 												id="email"
 												placeholder="Email"
-												class="w-100 mt-2 py-3 px-3 rounded-lg bg-white border-2 border-gray-400 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
+												className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border-2 border-gray-400 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
 											/>
 										</div>
 
-										<div class="flex flex-col mt-2">
-											<label for="tel" class="hidden">
+										<div className="flex flex-col mt-2">
+											<label for="tel" className="hidden">
 												Number
 											</label>
 											<input
@@ -52,13 +52,13 @@ export default function Portfolio() {
 												name="tel"
 												id="tel"
 												placeholder="Telephone Number"
-												class="w-100 mt-2 py-3 px-3 rounded-lg bg-white border-2 border-gray-400 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
+												className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border-2 border-gray-400 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
 											/>
 										</div>
 
 										<button
 											type="submit"
-											class="md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300"
+											className="md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300"
 										>
 											Submit
 										</button>

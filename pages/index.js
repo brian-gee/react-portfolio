a1b9c099c49aa6c58f0403e7c554a6e9e8ee7db9
brian-gee <brian86753099@gmail.com>
2023-01-08 00:14:17 -0500

    updating text color and verbiage

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,8 @@ export default function Home() {
 						<div className="text-left p-10">
 							<h2 className="text-4xl sm:text-5xl text-gray-800 font-extrabold tracking-tight py-2">Brian Geertsma</h2>
 							<h3 className="text-lg md:text-2xl py-2">Fullstack Developer</h3>
-							<p className="text-xs md:text-sm py-5 leading-8 max-w-xl">
-								As a South Florida-based developer, I am dedicated to creating
-								projects that streamline tasks and bring more purpose
-								and fulfillment to our daily lives.
+							<p className="text-gray-600 text-xs md:text-sm py-5 leading-8 max-w-xl">
+							Miami based software developer dedicated to creating projects that streamline tasks and enrich our daily lives.
 							</p>
 						</div>
 						<div className="grid justify-end">

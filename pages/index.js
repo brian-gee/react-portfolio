cb6894f0285521581952a200972b9d5f0f113ff6
Brian Geertsma <briange@tuta.io>
2023-01-26 11:44:34 -0500

    refactoring

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import bg from '../public/bg.jpg';
 import Portfolio from '../components/portfolio';
 import Contact from '../components/contact';
-import { BsLinkedin, BsGithub } from 'react-icons/bs';
+import About from '../components/about';
 
 export default function Home() {
   return (
@@ -13,55 +11,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <section className="flex justify-center pt-20">
-          <div className="flex justify-between max-w-7xl px-12">
-            <div className="text-left p-10">
-              <h2 className="text-4xl sm:text-5xl text-gray-800 font-extrabold tracking-tight py-2">
-                Brian Geertsma
-              </h2>
-              <h3 className="text-lg md:text-2xl py-2">Fullstack Developer</h3>
-              <p className="text-gray-600 text-xs md:text-sm py-5 leading-8 max-w-xl">
-                Miami based software developer dedicated to creating projects
-                that streamline tasks and enrich our daily lives.
-              </p>
-              <div className="text-5xl flex justify-start py-3 space-x-10">
-                <a
-                  href="https://github.com/brian-gee"
-                  className="hover:text-6xl">
-                  <BsGithub />
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/brian-geertsma/"
-                  className="hover:text-6xl text-blue-500">
-                  <BsLinkedin />
-                </a>
-              </div>
-            </div>
-            <div className="grid justify-end">
-              <Image
-                src={bg}
-                alt="bg"
-                className="rounded-xl"
-                width={300}
-                height={300}
-                style={{
-                  maxWidth: '95%',
-                  height: 'auto',
-                }}
-              />
-            </div>
+				{/* About Section */}
+        <section id="about">
+          <div className="mt-20">
+            <About />
           </div>
         </section>
 
+				{/* Portfolio Section */}
         <section id="portfolio">
           <div className="mt-20">
-            <Portfolio></Portfolio>
+            <Portfolio />
           </div>
         </section>
 
+				{/* Contact Section */}
         <section id="contact">
           <div>
-            <Contact></Contact>
+            <Contact />
           </div>
         </section>
       </main>

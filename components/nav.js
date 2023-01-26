d87ceced100ec592507ae9054560268e7589643e
Brian Geertsma <briange@tuta.io>
2023-01-26 11:39:17 -0500

    moving head to index page

--- components/nav.js
+++ components/nav.js
@@ -1,9 +1,8 @@
-import Head from 'next/head';
-import Link from 'next/link';
 import Image from 'next/image';
 import bgLogo from '../public/bg-logo.png';
 import { useState } from 'react';
 import { BsFillMoonStarsFill } from 'react-icons/bs';
+import { Button } from '@mui/material';
 
 export default function Nav() {
   const [navItems] = useState([
@@ -14,25 +13,23 @@ export default function Nav() {
   ]);
   return (
     <div>
-      <Head>
-        <title>Brian Gee</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="bg-white px-10">
-        <nav className="py-10 mb-12 flex justify-between">
+      <div className="bg-white p-10">
+        <nav className="flex justify-between">
           <div>
-            <Image src={bgLogo} alt="BG Logo" width={75} height={75}></Image>
+            <Image src={bgLogo} alt="BG Logo" width={80} height={80}></Image>
           </div>
           <div>
             <ul className="flex items-center">
               <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
               {navItems.map((link) => (
                 <li key={link.name}>
-                  <Link
+                  <Button
                     href={link.url}
-                    className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white px-4 py-2 rounded-md ml-6">
+                    className="ml-4"
+                    variant="contained"
+                    style={{ backgroundColor: '#3b82f6', color: '#FFFFFF' }}>
                     {link.name}
-                  </Link>
+                  </Button>
                 </li>
               ))}
             </ul>
--- pages/index.js
+++ pages/index.js
@@ -3,70 +3,68 @@ import Image from 'next/image';
 import bg from '../public/bg.jpg';
 import Portfolio from '../components/portfolio';
 import Contact from '../components/contact';
-import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
 
 export default function Home() {
-	return (
-		<div>
-			<Head>
-				<title>Brian Gee</title>
-			</Head>
+  return (
+    <div>
+      <Head>
+        <title>Brian Gee</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <section className="flex justify-center pt-20">
+          <div className="flex justify-between max-w-7xl px-12">
+            <div className="text-left p-10">
+              <h2 className="text-4xl sm:text-5xl text-gray-800 font-extrabold tracking-tight py-2">
+                Brian Geertsma
+              </h2>
+              <h3 className="text-lg md:text-2xl py-2">Fullstack Developer</h3>
+              <p className="text-gray-600 text-xs md:text-sm py-5 leading-8 max-w-xl">
+                Miami based software developer dedicated to creating projects
+                that streamline tasks and enrich our daily lives.
+              </p>
+              <div className="text-5xl flex justify-start py-3 space-x-10">
+                <a
+                  href="https://github.com/brian-gee"
+                  className="hover:text-6xl">
+                  <BsGithub />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/brian-geertsma/"
+                  className="hover:text-6xl text-blue-500">
+                  <BsLinkedin />
+                </a>
+              </div>
+            </div>
+            <div className="grid justify-end">
+              <Image
+                src={bg}
+                alt="bg"
+                className="rounded-xl"
+                width={300}
+                height={300}
+                style={{
+                  maxWidth: '95%',
+                  height: 'auto',
+                }}
+              />
+            </div>
+          </div>
+        </section>
 
-			<main>
-				<section className="flex justify-center pt-20">
-					<div className="flex justify-between max-w-7xl px-12">
-						<div className="text-left p-10">
-							<h2 className="text-4xl sm:text-5xl text-gray-800 font-extrabold tracking-tight py-2">
-								Brian Geertsma
-							</h2>
-							<h3 className="text-lg md:text-2xl py-2">Fullstack Developer</h3>
-							<p className="text-gray-600 text-xs md:text-sm py-5 leading-8 max-w-xl">
-								Miami based software developer dedicated to creating projects
-								that streamline tasks and enrich our daily lives.
-							</p>
-							<div className="text-5xl flex justify-start py-3 space-x-10">
-								<a
-									href="https://github.com/brian-gee"
-									className="hover:text-6xl"
-								>
-									<BsGithub />
-								</a>
-								<a
-									href="https://www.linkedin.com/in/brian-geertsma/"
-									className="hover:text-6xl text-blue-500"
-								>
-									<BsLinkedin />
-								</a>
-							</div>
-						</div>
-						<div className="grid justify-end">
-							<Image
-								src={bg}
-								alt="bg"
-								className="rounded-xl"
-								width={300}
-								height={300}
-								style={{
-									maxWidth: '95%',
-									height: 'auto',
-								}}
-							/>
-						</div>
-					</div>
-				</section>
+        <section id="portfolio">
+          <div className="mt-20">
+            <Portfolio></Portfolio>
+          </div>
+        </section>
 
-				<section id="portfolio">
-					<div className="mt-20">
-						<Portfolio></Portfolio>
-					</div>
-				</section>
-
-				<section id="contact">
-					<div>
-						<Contact></Contact>
-					</div>
-				</section>
-			</main>
-		</div>
-	);
+        <section id="contact">
+          <div>
+            <Contact></Contact>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }

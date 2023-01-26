429d847d7e619e28d953ef008fc8d71939a2188c
Brian Geertsma <briange@tuta.io>
2023-01-26 13:15:19 -0500

    updating for mobile first design

--- components/portfolio.js
+++ components/portfolio.js
@@ -1,23 +1,23 @@
-import { useState } from "react";
-import PortfolioItem from "./portfolioItem";
-import bwp from "../public/bwp.png";
-import wd from "../public/wd.png";
+import { useState } from 'react';
+import PortfolioItem from './portfolioItem';
+import bwp from '../public/bwp.png';
+import wd from '../public/wd.png';
 
 export default function Portfolio() {
   const [portfolioItems] = useState([
     {
-      name: "Customer Manager",
-      desc: "The customer manager web app is a powerful tool for businesses to manage their customer information. It is built with React, TypeScript, and Tailwind CSS, and uses Firebase for its backend.",
+      name: 'Blue Water Pools',
+      desc: 'Customer management page developed using React, TypeScript, and Firebase.',
       image: bwp,
-      github: "https://github.com/brian-gee/blue-water-pools",
-      link: "https://bluewaterpools.xyz",
+      github: 'https://github.com/brian-gee/blue-water-pools',
+      link: 'https://bluewaterpools.xyz',
     },
     {
-      name: "Weather Dashboard",
-      desc: "Our weather dashboard is a dynamic and intuitive tool for checking the current and forecasted weather conditions. It is built using JavaScript, HTML, and CSS on the frontend, and utilizes Node.js on the backend. The app also integrates with the OpenWeather API to retrieve live weather data for locations around the world.",
+      name: 'Weather Dashboard',
+      desc: 'Get realtime weather data using OpenWeatherAPI',
       image: wd,
-      github: "https://github.com/brian-gee/weather-dashboard",
-      link: "https://brian-gee.github.io/weather-dashboard/",
+      github: 'https://github.com/brian-gee/weather-dashboard',
+      link: 'https://brian-gee.github.io/weather-dashboard/',
     },
   ]);
 
@@ -26,7 +26,9 @@ export default function Portfolio() {
       <main className="relative flex items-top justify-center min-h-screen sm:items-center sm:pt-0">
         <section>
           <div className="text-center">
-            <h3 className="text-4xl sm:text-5xl text-gray-800 font-extrabold tracking-tight py-2">Portfolio</h3>
+            <h3 className="text-4xl sm:text-5xl text-gray-800 font-extrabold tracking-tight py-2">
+              Portfolio
+            </h3>
           </div>
           <div className="flex justify-center">
             <PortfolioItem portfolioItems={portfolioItems} />
--- components/portfolioItem.js
+++ components/portfolioItem.js
@@ -5,50 +5,48 @@ import AirplayIcon from '@mui/icons-material/Airplay';
 import Stack from '@mui/material/Stack';
 
 export default function PortfolioItem(params) {
-	const { portfolioItems } = params;
+  const { portfolioItems } = params;
 
-	return (
-		<div className="max-w-4xl">
-			{portfolioItems.map((portfolioItem, i) => (
-				<div key={i} className="grid grid-cols-2 shadow-lg m-5">
-					<div className="px-6 py-4">
-						<div className="text-lg md:text-2xl">
-							{portfolioItem.name}
-						</div>
-						<div className="text-xs md:text-sm text-gray-600">
-							{portfolioItem.desc}
-						</div>
-						<div className="p-2 mt-2">
-							<Stack direction="row" spacing={2}>
-								<Button
-									href={portfolioItem.link}
-									variant="contained"
-									style={{ backgroundColor: '#3b82f6', color: '#FFFFFF' }}
-									endIcon={<AirplayIcon />}
-								>
-									Live Demo
-								</Button>
-								<Button
-									href={portfolioItem.github}
-									variant="outlined"
-									size="small"
-									style={{ color: '#3b82f6' }}
-									startIcon={<GitHubIcon />}
-								>
-									Source Code
-								</Button>
-							</Stack>
-						</div>
-					</div>
-					<div className="px-4">
-						<Image
-							className="object-contain h-60 m-3"
-							src={portfolioItem.image}
-							alt={portfolioItem.name}
-						/>
-					</div>
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className="max-w-4xl">
+      {portfolioItems.map((portfolioItem, i) => (
+        <div key={i} className="shadow-lg m-5">
+          <div className="px-6 py-4">
+            <div className="text-center text-lg md:text-2xl">
+              {portfolioItem.name}
+            </div>
+            <div className="text-xs md:text-sm text-gray-600">
+              {portfolioItem.desc}
+            </div>
+            <div>
+              {' '}
+              <Image
+                className="object-contain m-3"
+                src={portfolioItem.image}
+                alt={portfolioItem.name}
+              />
+            </div>
+            <div className="p-2 mt-2">
+              <Stack direction="row" spacing={2}>
+                <Button
+                  href={portfolioItem.link}
+                  variant="contained"
+                  style={{ backgroundColor: '#3b82f6', color: '#FFFFFF' }}>
+                  Live Demo
+                </Button>
+                <Button
+                  href={portfolioItem.github}
+                  variant="outlined"
+                  style={{ color: '#3b82f6' }}
+                  startIcon={<GitHubIcon />}>
+                  Source Code
+                </Button>
+              </Stack>
+            </div>
+          </div>
+          <div className="px-4"></div>
+        </div>
+      ))}
+    </div>
+  );
 }
